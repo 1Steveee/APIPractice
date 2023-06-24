83cda7b60503d1b3b0766982734c747992d04cf8
1steveee <truckersteve781@gmail.com>
2023-06-23 22:39:13 -0400

    Get Request

---
+++ src/test/java/Chapter1Test.java
@@ -0,0 +1,20 @@
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Chapter1Test {
+
+    @Test
+    public void testRequestGetPlaceName() {
+        final String SITE = "http://api.zippopotam.us/us/33803";
+        
+
+        given().
+                when().
+                get(SITE).
+                then().
+                assertThat().
+                body("places[0].'place name'", equalTo("Lakeland"));
+    }
+}
